186cf9d821d4ed5cf2833247a1485d510aa97dce
Fenysk <minefenysk@gmail.com>
2024-11-18 10:00:50 +0100

    🐛 Add default value in pagination

--- src/common/dto/pagination.request.ts
+++ src/common/dto/pagination.request.ts
@@ -3,17 +3,17 @@ import { IsOptional, IsInt } from "class-validator";
 import { Transform, Type } from "class-transformer";
 
 export class PaginationRequest {
-    @ApiProperty({ required: false, description: 'Page number for pagination' })
+    @ApiProperty({ required: false, description: 'Page number for pagination', default: 1 })
     @IsOptional()
     @IsInt()
     @Type(() => Number)
     @Transform(({ value }) => parseInt(value))
-    page: number;
+    page: number = 1;
 
-    @ApiProperty({ required: false, description: 'Number of items per page' })
+    @ApiProperty({ required: false, description: 'Number of items per page', default: 10 })
     @IsOptional()
     @IsInt()
     @Type(() => Number)
     @Transform(({ value }) => parseInt(value))
-    limit: number;
+    limit: number = 10;
 }
